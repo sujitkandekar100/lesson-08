04f45d44a1a34e6d1519f800746f60ea670af98b
sujitkandekar100 <sujitkandekar100@gmail.com>
2025-01-01 09:45:44 +0530

    now learn addeventlistener and input element through value

--- domscripts.js
+++ domscripts.js
@@ -9,3 +9,19 @@ headButton.addEventListener('click',()=>{
 tailButton.addEventListener('click',()=>{
     paraGraph.innerHTML='You Choose: Tails';
 })
+const inputName=document.querySelector('.js-input-name');
+const submitButton=document.querySelector('.js-button-submit');
+const paraGraphName=document.querySelector('.js-paragraph-name');
+function showName(){
+    let name=inputName.value;
+    paraGraphName.innerHTML=`Your Name is : ${name}`;
+}
+submitButton.addEventListener('click',
+    showName
+)
+inputName.addEventListener('Keydown',(event)=>{
+    if(event.key==='Enter'){
+        showName();
+    }
+    
+})
