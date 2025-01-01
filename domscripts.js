0d11fc0b85c2cf7b7991cff54cbce08a0d5e26b2
sujitkandekar100 <sujitkandekar100@gmail.com>
2025-01-01 08:49:10 +0530

    created two button head and tails then when select head button show a you choose: Head

--- domscripts.js
+++ domscripts.js
@@ -1,2 +1,11 @@
 console.log(document.querySelector('button'));
 document.querySelector('.button-9a').innerHTML="9b done";
+const headButton=document.querySelector('.js-button-head');
+const tailButton=document.querySelector('.js-button-tails');
+const paraGraph=document.querySelector('.js-paragraph');
+headButton.addEventListener('click',()=>{
+          paraGraph.innerHTML='You Choose: Head';
+})
+tailButton.addEventListener('click',()=>{
+    paraGraph.innerHTML='You Choose: Tails';
+})
