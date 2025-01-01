8e28d7681f7ba8885641c12f8af367d0548ef4c3
sujitkandekar100 <sujitkandekar100@gmail.com>
2025-01-01 21:25:35 +0530

    when add to cart button click that time show message is added after two second remove message

--- advanced-function.js
+++ advanced-function.js
@@ -12,3 +12,11 @@ function fun(){
 setTimeout(()=>{
     document.querySelector('.js-button-start').innerHTML='Finished!';
 },2000);
+const addButton=document.querySelector('.js-button-add-to-cart');
+addButton.addEventListener('click',()=>{
+    const message=document.querySelector('.js-paragraph-added-message');
+    message.classList.add('css-added-message-show');
+    setTimeout(()=>{
+        message.classList.remove('css-added-message-show');
+    },2000);
+})
